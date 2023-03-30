de5a8c17a414e8377bb99e6414bfaa07bb0e5dfe
svet02-ai <snavas@correo.unimet.edu.ve>
2023-03-29 21:50:24 -0400

    interfaz

--- ProjectMendeley/src/projectmendeley/Main.java
+++ ProjectMendeley/src/projectmendeley/Main.java
@@ -14,7 +14,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        hashTable hs = new hashTable();
+        
+        Interfaz ventana = new Interfaz();
+        ventana.setVisible(true);
     }
     
 }
